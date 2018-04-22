3dc3d65e7f340ff4ac61efab4532984e8bcf9e37
Grzegorz Borkowski <gb.grzegorzborkowski@gmail.com>
2018-04-22 23:21:56 +0200

    [LAB6] Process tagged and non-tagged judgments simultaneously

--- lab6/JudgmentsProcessor.py
+++ lab6/JudgmentsProcessor.py
@@ -1,12 +1,12 @@
-import re, collections, csv
+import re, collections, csv, requests, tqdm, pickle
 
 class JudgmentsProcessor():
 
-    def __init__(self, raw_judgments, raw_signatures, most_common_words_file_path, tagger_broken_list = []):
+    def __init__(self, raw_judgments, raw_signatures, most_common_words_tagged_file_path, most_common_words_nontagged_file_path):
         self.__raw_judgments__ = raw_judgments
         self.__raw_signatures__ = raw_signatures
-        self.__most_common_words_file_path__ = most_common_words_file_path
-        self.__tagger_broken_list__ = tagger_broken_list
+        self.__most_common_words_tagged_file_path__ = most_common_words_tagged_file_path
+        self.__most_common_words_nontagged_file_path__ = most_common_words_nontagged_file_path
         self.__signatures_regexes__ = {
             re.compile(r'A?C.*'): 'sprawy cywilne',
             re.compile(r'A?U.*'): 'sprawy z zakresu ubezpieczenia społecznego',
@@ -28,28 +28,74 @@ class JudgmentsProcessor():
         '''
         signatures_categories = self.__extract_signatures_categories__()
         judgments_signatures = list(zip(self.__raw_judgments__, signatures_categories))
-
-        # print ("Content before filter_based_on_tagger_list" + str(((judgments_signatures)[:10])))
-        # judgments_signatures = self.__filter_based_on_tagger_list__(judgments_signatures)
-        # print ("Content after filter based on tagger list" + str(judgments_signatures))
         judgments_signatures_after_matching_signatures, not_matching_signatures = self.__match_signatures__(judgments_signatures)
         judgments_signatures = self.__filter_non_matching_codes__(judgments_signatures_after_matching_signatures)
         judgments_categories_counter = self.__count_judgments_categories__(judgments_signatures)
         judgments_signatures = self.__filter_categories_with_few_occurences__(judgments_signatures)
-        most_common_words = self.__extract_most_common_words__(self.__most_common_words_file_path__)
-        judgments_signatures = self.__filter__all_decisions__(judgments_signatures, most_common_words)
+        judgments_signatures = self.__filter__all_decisions_containing_reason__(judgments_signatures)
 
-        X, Y, new_X = [], [], []
+        tagged_judgments_signatures = []
+        non_tagged_judgments_signatures = []
+        print ("Before tagging:" + str(len(judgments_signatures)))
+        ###############################
+        for pair in tqdm.tqdm(judgments_signatures):
+            result = self.__make_query_to_tager__(pair)
+            if result[0]:
+                non_tagged_judgments_signatures.append(pair)
+                tagged_judgments_signatures.append(result)
+        ###############################
+        print ("After tagging:"+ str(len(non_tagged_judgments_signatures)))
+        print ("After tagging:" + str(len(tagged_judgments_signatures)))
 
-        for pair in judgments_signatures:
-            X.append(pair[0])
-            Y.append(pair[1])
+        print ("Saving judgments to files")
 
-        for sublist in X:
+        with open("non_tagged_judgments.pickle", "wb") as file:
+            pickle.dump(non_tagged_judgments_signatures, file)
+
+        with open("tagged_judgments_signatures.pickle", "wb") as file:
+            pickle.dump(tagged_judgments_signatures, file)
+
+        judgments_categories_counter = self.__count_judgments_categories__(tagged_judgments_signatures)
+
+
+        most_common_words_tagged = self.__extract_most_common_words__(self.__most_common_words_tagged_file_path__)
+        most_common_words_non_tagged = self.__extract_most_common_words__(self.__most_common_words_nontagged_file_path__)
+
+        tagged_judgments_signatures =  self.__remove_most_common_words_from_judgments__(
+            tagged_judgments_signatures, most_common_words_tagged)
+        non_tagged_judgments_signatures = self.__remove_most_common_words_from_judgments__(
+            non_tagged_judgments_signatures, most_common_words_non_tagged)
+
+        X_tagged_tmp, Y_tagged, X_non_tagged_tmp, Y_non_tagged = [], [], [], []
+        X_tagged, X_non_tagged = [], []
+
+        for pair in tagged_judgments_signatures:
+            X_tagged_tmp.append(pair[0])
+            Y_tagged.append(pair[1])
+
+        for sublist in X_tagged_tmp:
             words = " ".join(sublist)
-            new_X.append(words)
+            X_tagged.append(words)
 
-        return new_X, Y, judgments_categories_counter
+        for pair in non_tagged_judgments_signatures:
+            X_non_tagged_tmp.append(pair[0])
+            Y_non_tagged.append(pair[1])
+
+        for sublist in X_non_tagged_tmp:
+            words = " ".join(sublist)
+            X_non_tagged.append(words)
+
+        return X_tagged, Y_tagged, X_non_tagged, Y_non_tagged, judgments_categories_counter
+
+    def __make_query_to_tager__(self, judgment_signature_content):
+        data_to_query = (",").join(judgment_signature_content[0])
+        r = requests.post(data=data_to_query.encode("utf-8"), url="http://localhost:9200")
+        response_text = r.text
+        splited_response = response_text.splitlines()
+        splited_response = [" ".join(x.replace("\t", " ").replace("none", "")[1:].split(":")[:2][:1]).replace(" ", ":")
+                            for x in splited_response if ":" in x]
+        splited_response = [x.split(":")[0] for x in splited_response]
+        return (splited_response, judgment_signature_content[1])
 
     def __extract_signatures_categories__(self):
         signatures_flatten = [item for sublist in self.__raw_signatures__ for item in sublist]
@@ -93,21 +139,21 @@ class JudgmentsProcessor():
                 filtered_judgments_signatures.append(pair)
         return filtered_judgments_signatures
 
-    def __filter_decision__(self, decision, most_common_words):
+    def __filter_decision_containing_reason__(self, decision):
         try:
             idx = decision.index('uzasadnienie')
             new_decision = decision[idx + 1:]
             new_decision = [word for word in new_decision
-                            if (not word.isdigit() and len(word) > 1 and word not in most_common_words)]
+                            if (not word.isdigit() and len(word) > 1)]
             return new_decision
         except ValueError:
-            pass
+            return None
 
-    def __filter__all_decisions__(self, judgs_signs, most_common_words):
+    def __filter__all_decisions_containing_reason__(self, judgs_signs):
         filtered_judgments_signatures = []
         for pair in judgs_signs:
             decision = pair[0]
-            new_decision = self.__filter_decision__(decision, most_common_words)
+            new_decision = self.__filter_decision_containing_reason__(decision)
             if new_decision:
                 filtered_judgments_signatures.append((new_decision, pair[1]))
         return filtered_judgments_signatures
@@ -126,12 +172,9 @@ class JudgmentsProcessor():
         print (most_common_words)
         return most_common_words
 
-    def __filter_based_on_tagger_list__(self, judgments_signature_list):
-        print ("tagger_broken_list" + str(self.__tagger_broken_list__))
-        print ("judgments_signature_list" + str(list(judgments_signature_list)))
-
-        if len(self.__tagger_broken_list__) != len(judgments_signature_list):
-            return judgments_signature_list
-        print ("Filtering!")
-        zipped = list(zip(judgments_signature_list, self.__tagger_broken_list__))
-        return list(filter(lambda x: x[1] is True, zipped))
+    def __remove_most_common_words_from_judgments__(self, judgments_signatures, most_common_words_list):
+        filtered_judgments = []
+        for pair in judgments_signatures:
+            judgment = [word for word in pair[0] if word not in most_common_words_list]
+            filtered_judgments.append((judgment, pair[1]))
+        return filtered_judgments
